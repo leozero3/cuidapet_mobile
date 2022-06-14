84f9dff180f880a5093370d0f5755204a541e353
Leonardo Gonzalez dos Santos <realleo2009@hotmail.com>
2022-06-14 17:13:00 -0300

    config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:cuidapet_mobile/app/app_module.dart';
 import 'package:cuidapet_mobile/app/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ModularApp(
       module: AppModule(),
