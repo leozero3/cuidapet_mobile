9e7cf6cd7fccfe1958b2dc9d96ac6dc24145b063
leoW <realleo2009@hotmail.com>
2023-07-04 17:37:56 -0300

    Criando e config projeto e suas dependências 8:50

---
+++ lib/app_module.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind<Object>> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [];
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:cuidapet_mobile/app_module.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() {
+  runApp(ModularApp(module: AppModule() , child: App));
+} 
+
