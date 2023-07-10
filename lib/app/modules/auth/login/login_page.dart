16f3a3a38a881e4ce1affc218d646fd517552187
leoW <realleo2009@hotmail.com>
2023-07-10 17:41:44 -0300

    Adicionando icones ao projeto - 6:00

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:cuidapet_mobile/app/core/ui/icons/cuidapet_icons.dart';
 import 'package:cuidapet_mobile/app/core/ui/widgets/cuidapet_text_form_field.dart';
 import 'package:flutter/material.dart';
 
@@ -16,24 +17,29 @@ class LoginPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
-         key: formKey,
+          key: formKey,
           child: Column(
             children: [
               CuidapetTextFormField(
                 label: 'Login',
                 controller: textController,
                 validator: (value) {
-                  if (value ==null || value.isEmpty) {
+                  if (value == null || value.isEmpty) {
                     return 'Valor obrigatorio';
-                    
                   }
                   return null;
                 },
               ),
-              Text(textController.text),ElevatedButton(onPressed: () {
-                formKey.currentState?.validate();
-                print(textController.text);
-              }, child: Text(textController.text),)
+              Text(textController.text),
+              ElevatedButton(
+                onPressed: () {
+                  formKey.currentState?.validate();
+                  print(textController.text);
+                },
+                child: Text(textController.text),
+              ),
+              Icon(Cuidapet.facebook),
+              Icon(Cuidapet.google),
             ],
           ),
         ),
