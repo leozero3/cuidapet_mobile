84b5515f860c809f5a67070bfbc86ffe644bff09
leo <realleo2009@hotmail.com>
2023-07-20 22:16:07 -0300

    Componente de Mensagem (Snackbar Asuka) - final

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,7 +1,6 @@
 import 'package:cuidapet_mobile/app/modules/auth/home/auth_home_page.dart';
 import 'package:cuidapet_mobile/app/modules/auth/login/login_module.dart';
 import 'package:cuidapet_mobile/app/modules/auth/register/register_module.dart';
-import 'package:cuidapet_mobile/app/modules/core/auth/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AuthModule extends Module {
