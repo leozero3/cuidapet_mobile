263f06c69e3ebca543228c0e584475a0d52fd24e
Leonardo Gonzalez dos Santos <59115521+leozero3@users.noreply.github.com>
2023-12-01 17:50:30 -0300

    # Configurando Sqlite 7:00

---
+++ lib/app/core/database/sql_connection_factory.dart
@@ -0,0 +1,11 @@
+class SqlConnectionFactory {
+  static const _version = 1;
+  static const _databaseName = 'CUIDAPET_LOCAL_DB';
+  static SqlConnectionFactory? instance;
+
+  SqlConnectionFactory._();
+
+  // factory SqlConnectionFactory() {
+  //   _in
+  // }
+}
