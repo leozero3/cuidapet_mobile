590917188fcf63ffe6ccd845c768d8fe677e08ee
Leo Home <realleo2009@hotmail.com>
2023-12-22 21:22:17 -0300

    #Construindo AppBar da HomePage

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,12 +1,8 @@
 import 'package:cuidapet_mobile/app/core/life_cycle/page_life_cycle_state.dart';
-import 'package:cuidapet_mobile/app/core/rest_client/rest_client.dart';
 import 'package:cuidapet_mobile/app/entities/address_entity.dart';
 import 'package:cuidapet_mobile/app/modules/home/home_controller.dart';
-import 'package:cuidapet_mobile/app/services/address/address_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cuidapet_mobile/app/modules/home/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,49 +16,16 @@ class _HomePageState extends PageLifeCycleState<HomeController, HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Column(
-        children: [
-          TextButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text('Logout')),
-          TextButton(
-              onPressed: () async {
-                final categoriesResponse =
-                    await Modular.get<RestClient>().auth().get('/categories/');
-                print(categoriesResponse.data);
-              },
-              child: const Text('Teste refresh token')),
-          TextButton(
-              onPressed: () async {
-                controller.goToAddressPage();
-              },
-              child: const Text('Ir para Endereço')),
-          TextButton(
-              onPressed: () async {
-                final address =
-                    await Modular.get<AddressService>().getAddressSelected();
-                setState(() {
-                  addressEntity = address;
-                });
-              },
-              child: const Text('Buscar endereco')),
-          Observer(builder: (_) {
-            return Text(
-              controller.addressEntity?.address ??
-                  'Nenhum endereco selecionado',
-            );
-          }),
-          Observer(builder: (_) {
-            return Text(controller.addressEntity?.additional ??
-                'Nenhum complemento selecionado');
-          })
-        ],
-      ),
-    );
+        drawer: Drawer(
+          backgroundColor: Colors.grey[100],
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              HomeAppBar(),
+            ];
+          },
+          body: Container(),
+        ));
   }
 }
---
+++ lib/app/modules/home/widgets/home_appbar.dart
@@ -0,0 +1,86 @@
+import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extension.dart';
+import 'package:cuidapet_mobile/app/core/ui/extensions/theme_extensions.dart';
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends SliverAppBar {
+  const HomeAppBar({super.key})
+      : super(
+          expandedHeight: 100,
+          collapsedHeight: 100,
+          elevation: 0,
+          flexibleSpace: const _CuidapetAppBar(),
+          iconTheme: const IconThemeData(color: Colors.black),
+          pinned: true,
+        );
+}
+
+class _CuidapetAppBar extends StatelessWidget {
+  const _CuidapetAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final outlineInputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(30),
+      borderSide: BorderSide(
+        color: Colors.grey[200]!,
+      ),
+    );
+    return AppBar(
+      backgroundColor: Colors.grey[100],
+      centerTitle: true,
+      title: const Padding(
+        padding: EdgeInsets.only(
+          bottom: 12.0,
+        ),
+        child: Text(
+          'Cuidapet',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.location_on,
+            color: Colors.black,
+          ),
+        ),
+      ],
+      elevation: 0,
+      flexibleSpace: Stack(
+        children: [
+          Container(
+            height: 110.h,
+            color: context.primaryColor,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              width: .9.sw,
+              child: Material(
+                elevation: 4,
+                borderRadius: BorderRadius.circular(30),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    suffixIcon: const Icon(
+                      Icons.search,
+                      size: 25,
+                      color: Colors.grey,
+                    ),
+                    border: outlineInputBorder,
+                    focusedBorder: outlineInputBorder,
+                    enabledBorder: outlineInputBorder,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
